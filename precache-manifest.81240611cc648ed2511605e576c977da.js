2c7004492b86e8053fd485bfe8079213fb583489
Soumyanetra <soumyanetrap.stcet@gmail.com>
2020-07-27 23:16:50 +0530

    Updates

---
+++ precache-manifest.81240611cc648ed2511605e576c977da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96463ffc2c1827d017a59835c919325b",
+    "url": "/Score-Handler/index.html"
+  },
+  {
+    "revision": "423a1d6f19d246f515d5",
+    "url": "/Score-Handler/static/css/main.adcb6708.chunk.css"
+  },
+  {
+    "revision": "5f38387841e012c56837",
+    "url": "/Score-Handler/static/js/2.dda6e2a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Score-Handler/static/js/2.dda6e2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423a1d6f19d246f515d5",
+    "url": "/Score-Handler/static/js/main.22862ca4.chunk.js"
+  },
+  {
+    "revision": "8c91c51c3cbb703388fc",
+    "url": "/Score-Handler/static/js/runtime-main.899f5b96.js"
+  }
+]);
